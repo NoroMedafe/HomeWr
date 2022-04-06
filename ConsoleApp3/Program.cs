caeac31bfd3c73262797bdfa8f1e5bf10a628090
NoroMedafe <danya1515@inbox.ru>
2022-04-06 14:28:52 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowLenght = 3;
+            int countPicture = 52;
+            int rowCount;
+            int extraPictures;
+
+            rowCount = 52 / 3;
+            extraPictures = 52 % 3;
+            Console.WriteLine("рядов заполнено:" + rowCount + " лишних картин: " + extraPictures);
+            Console.ReadKey();
+        }
+    }
+}
